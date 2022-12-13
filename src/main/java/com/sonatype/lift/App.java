d5951055c940665aa690a3df246465a8e7494a37
Matt Johnson <mjohnson@sonatype.com>
2022-12-13 09:10:31 -0500

    JavaUtilDate

--- src/main/java/com/sonatype/lift/App.java
+++ src/main/java/com/sonatype/lift/App.java
@@ -1,13 +1,41 @@
 package com.sonatype.lift;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  */
 public class App
 {
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World!");
+
+  private static final Map<String, Integer> CAL_MAP = new HashMap<String, Integer>();
+
+  static {
+    CAL_MAP.put("year", Calendar.YEAR);
+    CAL_MAP.put("month", Calendar.MONTH);
+    CAL_MAP.put("date", Calendar.DATE);
+    CAL_MAP.put("dayOfMonth", Calendar.DATE);
+    CAL_MAP.put("hourOfDay", Calendar.HOUR_OF_DAY);
+    CAL_MAP.put("minute", Calendar.MINUTE);
+    CAL_MAP.put("second", Calendar.SECOND);
+  }
+
+  public static void set(Date self, Map<Object, Integer> updates) {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(self);
+    set(cal, updates);
+    self.setTime(cal.getTimeInMillis());
+  }
+
+  public static void set(Calendar self, Map<Object, Integer> updates) {
+    for (Map.Entry<Object, Integer> entry : updates.entrySet()) {
+      Object key = entry.getKey();
+      if (key instanceof String) key = CAL_MAP.get(key);
+      if (key instanceof Integer) self.set((Integer) key, entry.getValue());
+    }
   }
 
   static class Data1
