ec8ed191a6f541de08b20c94088d7ca72eca7d39
Matt Johnson <mjohnson@sonatype.com>
2022-12-23 11:07:42 -0500

    Make it work

--- src/main/java/com/sonatype/lift/App.java
+++ src/main/java/com/sonatype/lift/App.java
@@ -1,18 +1,11 @@
 package com.sonatype.lift;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Hello world!
  */
 public class App
 {
-
-  public static void main(String args[]) {
-    System.out.println("Hello, world!")
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
   }
-
 }
