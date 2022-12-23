7cf510b1a30d544caa969f19a0756682facc1fda
Matt Johnson <mjohnson@sonatype.com>
2022-12-23 14:03:07 -0500

    Update src/main/java/com/sonatype/lift/App.java
    Co-authored-by: localhost-muse-bot[bot] <64798418+localhost-muse-bot[bot]@users.noreply.github.com>

--- src/main/java/com/sonatype/lift/App.java
+++ src/main/java/com/sonatype/lift/App.java
@@ -19,7 +19,7 @@ public class App
 
     @Override
     public boolean equals(Object other) {
-      Data1 that = (Data1) other; // BAD: This may throw ClassCastException.
+      if (!(other instanceof Data1)) { return false; }Data1 that = (Data1) other;
       return a == that.a;
     }
   }
